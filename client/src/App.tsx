6dac404e5ada83402450be8b598daadb58201d74
Debatreya <debatreyadas@gmail.com>
2024-03-01 17:06:26 +0530

    React+TS+Tailwind+ShadCN initialised

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sample from './sample'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 text-2xl font-semibold text-center'>Hello This is STATUS-200 🟢</h1>
+      <h2 className='font-mono font-bold text-center'>DESIGN - DEPOLY - DOMINATE</h2>
+      <p className='text-center'><Sample /></p> 
+    </>
+  )
+}
+
+export default App
