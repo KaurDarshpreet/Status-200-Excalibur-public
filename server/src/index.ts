ac2eb672a2544847615d6b3a42e905ce7e3bc123
sanyam <sanyam9922@gmail.com>
2024-03-02 16:04:44 +0530

    prisma-schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,21 +3,10 @@ import { PrismaClient } from '@prisma/client'
 import "dotenv/config";
 const prisma = new PrismaClient()
 const app: Express = express();
-const post = 
+
 app.use(express.json());
 app.get("/",async (req: Request, res: Response)=>{
-    await prisma.student.deleteMany();
-    const user = await prisma.student.create({
-        data : {
-            domain_id : "1234@nitkkr.ac.in",
-            password : "1234",
-            name : "user-1",
-            hostel : "h-10",
-            room_number : "200",
-            phone_number : 123456789
-        }
-    });
-    res.json(user);
+    res.json("Express server");
 });
 
 app.listen(5000, ()=>{
