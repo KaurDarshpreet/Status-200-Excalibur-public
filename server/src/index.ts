a98bdede0744b0e6ba11349a369691290e3453bf
sanyam <sanyam9922@gmail.com>
2024-03-02 16:00:54 +0530

    prisma-client-generated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import express, {Express, Request, Response} from "express"
-
+import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+const prisma = new PrismaClient()
 const app: Express = express();
+const post = 
 app.use(express.json());
-app.get("/", (req: Request, res: Response)=>{
-    res.send("express application");
+app.get("/",async (req: Request, res: Response)=>{
+    await prisma.student.deleteMany();
+    const user = await prisma.student.create({
+        data : {
+            domain_id : "1234@nitkkr.ac.in",
+            password : "1234",
+            name : "user-1",
+            hostel : "h-10",
+            room_number : "200",
+            phone_number : 123456789
+        }
+    });
+    res.json(user);
 });
 
 app.listen(5000, ()=>{
