52b4b6f1687c8b8edc292a257428698c2df867c2
Debatreya <debatreyadas@gmail.com>
2024-03-04 13:14:43 +0530

    Issue form created

--- client/src/pages/Student/StudentDash.tsx
+++ client/src/pages/Student/StudentDash.tsx
@@ -1,7 +1,12 @@
+import IssueForm from "./IssueForm"
+
 const StudentDash = () => {
     return (
-        <div>
-        <h1>Student Dashboard</h1>
+        <div className="container flex items-center gap-4 justify-center min-w-[100svw] min-h-[100svh] bg-slate-600">
+            <div className="profile flex flex-col items-center bg-black min-w-[18svw] min-h-[94svh] rounded-md"></div>
+            <div className="post_issue min-h-[94svh] min-w-[78svw] bg-blue-950 rounded-md flex justify-between items-center">
+                <IssueForm />
+            </div>
         </div>
     )
 }
