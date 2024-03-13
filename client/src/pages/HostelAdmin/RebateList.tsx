101891d9e9a8032b663170c9ac236f5e99bc1ea7
Debatreya <debatreyadas@gmail.com>
2024-03-13 12:21:23 +0530

    I dont know what am i commiting #2

--- client/src/pages/HostelAdmin/RebateList.tsx
+++ client/src/pages/HostelAdmin/RebateList.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import React, { useState } from "react"
+import { useState } from "react"
 
 export default function RebatePage(){
   const [rebateDetails, setRebateDetails] = useState([
